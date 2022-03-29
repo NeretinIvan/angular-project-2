a0fde1339cf5e67106f89985aba6ee70c33864a1
Ivan Neretin <neretinivan17@yandex.ru>
2022-03-29 19:25:06 +0300

    homework-5 done

--- angular-project2/src/app/timer.service.ts
+++ angular-project2/src/app/timer.service.ts
@@ -1,13 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import { DOCUMENT, SET_INTERVAL_FUNCTION } from './app.module';
+
+export interface DocumentInterface {
+  querySelector(tag: string);
+}
+export type SetIntervalFunction = (callback: () => void, ms: number, ...args: any[]) => any;
 
 @Injectable()
 export class TimerService {
-  constructor() {}
+  constructor(
+    @Inject(DOCUMENT) private document: DocumentInterface,
+    @Inject(SET_INTERVAL_FUNCTION) public setInterval: SetIntervalFunction
+  ) {}
 
   public start() {
-    const timerOutputElement = document.querySelector('#timer-output');
+    const timerOutputElement = this.document.querySelector('#timer-output');
     let counter: number = 0;
-    setInterval(() => {
+    this.setInterval(() => {
       timerOutputElement.innerHTML = `${counter++}`;
     }, 1000);
   }
